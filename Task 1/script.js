edf4ee87597adbea60a7abc75da9e1471ca9702a
Hareen-P <146912688+Hareen-P@users.noreply.github.com>
2024-12-15 11:43:12 +0530

    Add files via upload

---
+++ Task 1/script.js
@@ -0,0 +1,8 @@
+// Select the button element
+const alertButton = document.getElementById("alertButton");
+
+// Add an event listener to the button
+alertButton.addEventListener("click", function() {
+    // Show an alert message when the button is clicked
+    alert("Space is vast, mysterious, and waiting to be explored!");
+});
